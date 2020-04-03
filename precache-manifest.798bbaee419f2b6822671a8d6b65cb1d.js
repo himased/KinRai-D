037c2e8099673e71f862c81d1dd2d6dc142a7f9a
himased1214 <chanakan_c@elearning.cmu.ac.th>
2020-04-03 20:59:38 +0700

    Updates

---
+++ precache-manifest.798bbaee419f2b6822671a8d6b65cb1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9181ae62fc2f1e9c51baf867f161500",
+    "url": "/KinRai-D/index.html"
+  },
+  {
+    "revision": "d23ed203babfceef35d9",
+    "url": "/KinRai-D/static/css/main.b312a01f.chunk.css"
+  },
+  {
+    "revision": "ca9e59591085828401c1",
+    "url": "/KinRai-D/static/js/2.6665392d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KinRai-D/static/js/2.6665392d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d23ed203babfceef35d9",
+    "url": "/KinRai-D/static/js/main.7def570e.chunk.js"
+  },
+  {
+    "revision": "a80a0b10c17f48d45ddb",
+    "url": "/KinRai-D/static/js/runtime-main.3ac49518.js"
+  },
+  {
+    "revision": "0ef117eed1c793ec0f5b1d6a3b77f92b",
+    "url": "/KinRai-D/static/media/lemillion.0ef117ee.jpg"
+  }
+]);
